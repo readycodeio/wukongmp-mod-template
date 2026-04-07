4ab1223bdbee132b49f88e1562f855fa4d81a367
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-04-07 15:16:10 +0200

    Add RPC example

--- ExampleMod/Mod.cs
+++ ExampleMod/Mod.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
-using ReadyM.Api.Command;
+﻿using ReadyM.Api.Command;
 using ReadyM.Api.DI;
+using ReadyM.Api.Idents;
+using ReadyM.Api.Multiplayer.Client;
+using ReadyM.Api.Multiplayer.Generators;
+using ReadyM.Api.Multiplayer.Protocol.Enums;
+using ReadyM.Api.Multiplayer.RPC;
+using ReadyM.Api.Multiplayer.Serialization;
+using UnrealEngine.Runtime;
 using WukongMp.Sdk;
 using WukongMp.Sdk.Api;
 
@@ -15,10 +21,24 @@ public class Mod : ModBase
     protected override void Initialize(IDependencyContainer services)
     {
         // register and resolve your services here, for example:
-        // services.RegisterSingleton<IMyService, MyService>();
-        // var myService = services.Resolve<IMyService>();
+        services.RegisterSingleton<ExampleRpc>();
+        var rpc = services.Resolve<ExampleRpc>();
 
         // use the WukongApi class to interact with the SDK, for example:
-        WukongApi.Console.AddCommand("example_command", ConsoleCommand.Create(() => { Logger.LogInformation("Example command executed!"); }));
+        WukongApi.Console.AddCommand("example_command", ConsoleCommand.Create(() =>
+        {
+            WukongApi.Local.AddChatMessage("Example command executed!", FLinearColor.Orange);
+            rpc.SendExampleEvent("Hello from the example command!");
+        }));
+    }
+}
+
+public partial class ExampleRpc(IRpcClient client, IRelaySerializer serializer) : RpcClassBase(client, serializer)
+{
+    // define your RPC methods here, for example:
+    [RpcEvent(RelayMode.AreaOfInterestAll)]
+    private void OnExampleEvent(PlayerId __sender, string message)
+    {
+        WukongApi.Local.AddChatMessage($"Received message from {__sender}: {message}", FLinearColor.Green);
     }
 }
