0f813d3fb57ecd949d72d11f9c8583c3c90d6bee
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-05-06 16:36:04 +0200

    Update references

--- ExampleMod/Mod.cs
+++ ExampleMod/Mod.cs
@@ -1,4 +1,5 @@
-﻿using ReadyM.Api.Command;
+﻿using CSharpModBase.Input;
+using ReadyM.Api.Command;
 using ReadyM.Api.DI;
 using ReadyM.Api.Idents;
 using ReadyM.Api.Multiplayer.Client;
@@ -6,9 +7,13 @@ using ReadyM.Api.Multiplayer.Generators;
 using ReadyM.Api.Multiplayer.Protocol.Enums;
 using ReadyM.Api.Multiplayer.RPC;
 using ReadyM.Api.Multiplayer.Serialization;
+using UnrealEngine.Engine;
 using UnrealEngine.Runtime;
+using WukongMp.Api;
+using WukongMp.Api.Configuration;
 using WukongMp.Sdk;
 using WukongMp.Sdk.Api;
+using HarmonyLib;
 
 namespace ExampleMod;
 
@@ -28,15 +33,29 @@ public class Mod : ModBase
             WukongApi.Chat.ShowLocalMessage("Example command executed!", FLinearColor.Orange);
             rpc.SendExampleEvent("Hello from the example command!");
         }));
+
+        // register input bindings, for example:
+        WukongApi.Input.RegisterKeyBind(Key.F5, () => { WukongApi.Chat.ShowLocalMessage("F5 key pressed!", FLinearColor.Blue); });
     }
 }
 
+// define your RPC methods, for example:
 public partial class ExampleRpc(IRpcClient client, IRelaySerializer serializer) : RpcClassBase(client, serializer)
 {
-    // define your RPC methods here, for example:
     [RpcEvent(RelayMode.AreaOfInterestAll)]
     private void OnExampleEvent(PlayerId __sender, string message)
     {
         WukongApi.Chat.ShowLocalMessage($"Received message from {__sender}: {message}", FLinearColor.Green);
+    }
+}
+
+// use Harmony to patch a game method, for example:
+[HarmonyPatch(typeof(UGameplayStatics), nameof(UGameplayStatics.OpenLevel))]
+[HarmonyPatchCategory(PatchCategory.Global)]
+public static class ExamplePatch
+{
+    public static void Postfix(FName LevelName)
+    {
+        Logging.LogDebug("Entering level: {LevelName}", LevelName.ToString());
     }
 }
